712244bff18633e31b4b35cf7da69317bc342b0b
Julie Henry <julie@juliehenrydesignllc.com>
2015-12-14 21:58:32 -0500

    began layout and scaffolding and started adding id's to html, started jQuery

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	$(function() {
+		$("#add").on("click",addItem);
+		function addItem() {
+		var text = $("#addtext").val();
+		alert(text);
+	
+	}
+
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
