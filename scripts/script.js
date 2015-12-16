38ce0b3abafa02989c57cd0011951edb5b2cd183
Julie Henry <julie@juliehenrydesignllc.com>
2015-12-16 13:32:47 -0500

    It still needs to be designed and tweeked, but it's functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
-$('#add').on('click', function(){
+$('.add').on('click', function(){
 	$("#g-list").append("<li>"+$('#new-text').val()+ "<button>DONE</button>"+"</li>");
 		$("#new-text").val("");
 	$("li").on("click", function(){
 	$(this).remove();
-})
+});
 
 });
 
 $("li").on("click", function(){
 	$(this).remove();
-})
+});
 
 
 
